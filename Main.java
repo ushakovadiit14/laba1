71bc1df1bc9b51f74c2b994eef65e6c5ef76a8cb
Diana Ushakova <dielluvv707@gmail.com>
2025-09-15 09:15:27 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Выберите задание (1-20): ");
+        methods utils = new methods(); // Создаем экземпляр класса methods
+
+        System.out.print("Выберите задание (1-20):\n (1) Задание 1.2 - Сумма последних цифр\n (2) Задание 1.4 - Проверка положительности\n " +
+                "(3) Задание 1.6 - Проверка регистра буквы\n (4)Задание 1.8 - Проверка делимости\n (5)Задание 1.10 - Сумма последних цифр двух чисел\n" +
+                " (6) Задание 2.2 - Безопасное деление\n (7)Задание 2.4 - Сравнение чисел\n (8)Задание 2.6 - Тройная сумма\n (9)Задание 2.8 - Склонение возраста\n " +
+                "(10) Задание 2.10 - Дни недели\n (11) Задание 3.2 - Обратный отсчет\n (12) Задание 3.4 - Возведение в степень\n (13) Задание 3.6 - Одинаковые цифры\n " +
+                "(14) Задание 3.8 - Треугольник из звездочек\n (15) Задание 3.10 - Игра \"Угадайка\" \n (16) Задание 4.2 - Поиск последнего вхождения\n" +
+                " (17) Задание 4.4 - Вставка в массив\n (18) Задание 4.6 - Реверс массива\n (19) Задание 4.8 - Объединение массивов\n (20) Задание 4.10 - Удаление отрицательных чисел");
+
         while (!scanner.hasNextInt()) {
             System.out.println("Ошибка! Введите цифры, а не текст.");
             System.out.print("Введите задание (1-20): ");
@@ -13,64 +21,64 @@ public class Main {
 
         switch (choice) {
             case 1:
-                methods.sumLastNums2();
+                utils.sumLastNums2();
                 break;
             case 2:
-                methods.isPositive2();
+                utils.isPositive2();
                 break;
             case 3:
-                methods.isUpperCase2();
+                utils.isUpperCase2();
                 break;
             case 4:
-                methods.isDivisor2();
+                utils.isDivisor2();
                 break;
             case 5:
-                methods.lastNumSum2();
+                utils.lastNumSum2();
                 break;
             case 6:
-                methods.safeDiv2();
+                utils.safeDiv2();
                 break;
             case 7:
-                methods.makeDecision2();
+                utils.makeDecision2();
                 break;
             case 8:
-                methods.sum32();
+                utils.sum32();
                 break;
             case 9:
-                methods.age2();
+                utils.age2();
                 break;
             case 10:
-                methods.printDays2();
+                utils.printDays2();
                 break;
             case 11:
-                methods.reverseListNums2();
+                utils.reverseListNums2();
                 break;
             case 12:
-                methods.pow2();
+                utils.pow2();
                 break;
             case 13:
-                methods.equalNum2();
+                utils.equalNum2();
                 break;
             case 14:
-                methods.leftTriangle2();
+                utils.leftTriangle2();
                 break;
             case 15:
-                methods.guessGame2();
+                utils.guessGame2();
                 break;
             case 16:
-                methods.findLast2();
+                utils.findLast2();
                 break;
             case 17:
-                methods.add2();
+                utils.add2();
                 break;
             case 18:
-                methods.reverse2();
+                utils.reverse2();
                 break;
             case 19:
-                methods.concat2();
+                utils.concat2();
                 break;
             case 20:
-                methods.deleteNegative2();
+                utils.deleteNegative2();
                 break;
             default:
                 System.out.println("Неверный выбор!");
